36f6e43a19caaf3c5402e5defd73c0b43f272bdc
HoangAn-B2003774 <115530751+HoangAn-B2003774@users.noreply.github.com>
2024-05-19 00:07:56 +0700

    update

---
+++ SQLQuery1.sql
@@ -0,0 +1,45 @@
+﻿CREATE DATABASE RegisterLoginDB;
+GO
+
+USE RegisterLoginDB;
+GO
+
+CREATE TABLE Roles (
+    RoleID INT PRIMARY KEY IDENTITY(1,1),
+    RoleName NVARCHAR(50) NOT NULL
+);
+GO
+
+-- Thêm một số vai trò mặc định
+INSERT INTO Roles (RoleName) VALUES ('Admin'), ('Publisher'), ('Reviewer'), ('Moderator');
+GO
+SELECT * FROM Roles
+
+
+CREATE TABLE Users (
+    UserID INT PRIMARY KEY IDENTITY(1,1),
+    Name NVARCHAR(100) NOT NULL,
+    Email NVARCHAR(100) NOT NULL UNIQUE,
+    PasswordHash NVARCHAR(255) NOT NULL,
+    RoleID INT FOREIGN KEY REFERENCES Roles(RoleID),
+	UpdatedAt DATETIME DEFAULT GETDATE(),
+	CreatedAt DATETIME DEFAULT GETDATE()
+);
+GO
+
+
+-- Trigger để cập nhật UpdatedAt
+-- Trigger để tự động cập nhật cột UpdatedAt mỗi khi có sự thay đổi.
+CREATE TRIGGER trgUsersUpdatedAt
+ON Users
+AFTER UPDATE
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    UPDATE Users
+    SET UpdatedAt = GETDATE()
+    FROM Users u
+    INNER JOIN inserted i ON u.UserID = i.UserID;
+END;
+GO
